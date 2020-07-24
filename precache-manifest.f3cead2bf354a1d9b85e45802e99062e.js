42ac271d2b1802c38ddb7a1a2b1180036c64eb95
jeffhundred <benboyer128@gmail.com>
2020-07-24 00:17:45 -0400

    Updates

---
+++ precache-manifest.f3cead2bf354a1d9b85e45802e99062e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27d6c2a8e676ad893b37ce7aebfef5a6",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "e02c78d9a2a0c1694fe8",
+    "url": "/my-portfolio/static/css/main.e3fc04e1.chunk.css"
+  },
+  {
+    "revision": "5538337958a410afd8c5",
+    "url": "/my-portfolio/static/js/2.f20292b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-portfolio/static/js/2.f20292b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e02c78d9a2a0c1694fe8",
+    "url": "/my-portfolio/static/js/main.4bd4901c.chunk.js"
+  },
+  {
+    "revision": "170f45050b91094c3a2e",
+    "url": "/my-portfolio/static/js/runtime-main.354f34ae.js"
+  },
+  {
+    "revision": "9ab6b52c5544e013511a3032edc3cce6",
+    "url": "/my-portfolio/static/media/arcade.9ab6b52c.jpg"
+  },
+  {
+    "revision": "76029a67adb5e1d4b4d75df5cb21ef54",
+    "url": "/my-portfolio/static/media/bike.76029a67.jpg"
+  },
+  {
+    "revision": "017a5d9501d852ab36f6689bd007c2f2",
+    "url": "/my-portfolio/static/media/coding.017a5d95.jpg"
+  },
+  {
+    "revision": "43fd3e51533169cbf859540971113058",
+    "url": "/my-portfolio/static/media/game.43fd3e51.jpg"
+  },
+  {
+    "revision": "c1f76401c2b7917fcb6c37d791aba3c5",
+    "url": "/my-portfolio/static/media/hill-path.c1f76401.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a78c3ebb83849bfb0594bd43a3aba0d5",
+    "url": "/my-portfolio/static/media/resume2020.a78c3ebb.pdf"
+  }
+]);
